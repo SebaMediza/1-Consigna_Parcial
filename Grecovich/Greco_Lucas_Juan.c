0f2f7bcd272666ee7f1a0c477a25f68bb86f4410
Sebastian Mediza <sebamediza@hotmail.com>
2022-10-03 13:48:06 -0300

    Cuarta_Iteracion

--- Grecovich/Greco_Lucas_Juan.c
+++ Grecovich/Greco_Lucas_Juan.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <windows.h>
 
-#define TOTTEMP 60 
+#define TOTTEMP 30 
 #define TEV1 5 
 #define MINTEM -25
 #define MAXTEM 50
@@ -30,8 +30,8 @@ int main(){
 		
 	tiempo_inicio = clock();
 	inicio_evento1 = clock(); 
-	int cont1=0, cont2=0, contac=0, contae=0;
-	bool control1, control2;
+	int contadorCalor=0, contadorEnfriador=0, contadorSuperCalentar=0, contadorSuperEnfriar=0;
+	bool controlCalor=false, controlEnfriar=false;
 	
 	while (tiempo_tot< TOTTEMP){ 
 		fin_evento1 = clock();
@@ -39,50 +39,51 @@ int main(){
 		segundos_v = (double)(fin_evento1-inicio_evento1)/ CLOCKS_PER_SEC;
 		tiempo_tot= (double)(tiempo_final-tiempo_inicio) / CLOCKS_PER_SEC;
 		
-		
 		if (segundos_v >=TEV1){
 			inicio_evento1 = fin_evento1;
 			int temp=0;
 			temp=sensa_temp();
 			printf("Temperatura: %d °C \n", temp);
 			if(temp<0){
-				printf("Calentar\n");
-				if(cont1<1){
-					cont1++;
-					control1=false;
-					calefactor(temp, control1);
-				}else{
-					if(cont1>=1){
-						cont1++;
-						control1=true;
-						calefactor(temp, control1);
-						contac++;
-					}
+				int nuevaTemperatura=0;
+				printf("Calentando...\n");
+				contadorCalor++;
+				if(contadorCalor==2){
+					contadorCalor=0;
+					controlCalor=true;
+					nuevaTemperatura=calefactor(temp, controlCalor);
+					contadorSuperCalentar++;
+					//printf("La nueva temperatura es: %d°C\n",nuevaTemperatura);
+					//break;
 				}
+				nuevaTemperatura=calefactor(temp, controlCalor);
+				contadorEnfriador=0;
+				printf("La nueva temperatura es: %d°C\n",nuevaTemperatura);
 			}else{
 				if(temp>40){
-					printf("Enfriar\n");
-					if(cont2<1){
-						cont2++;
-						control2=false;
-						enfriador(temp,control2);
-					}else{
-						if(cont1>=1){
-							cont2++;
-							control2=true;
-							enfriador(temp,control2);
-							contae++;
-						}
+					int nuevaTemperatura=0;
+					printf("Enfriando...\n");
+					contadorEnfriador++;					
+					if(contadorEnfriador==2){
+						contadorEnfriador=0;
+						controlEnfriar=true;
+						nuevaTemperatura=enfriador(temp,controlEnfriar);
+						contadorSuperEnfriar++;
+						//printf("La nueva temperatura es: %d°C\n",nuevaTemperatura);
+						//break;
 					}
+					nuevaTemperatura=enfriador(temp,controlEnfriar);
+					contadorCalor=0;
+					printf("La nueva temperatura es: %d°C\n",nuevaTemperatura);
 				}
 			}
-		
 		printf("Tiempo entre eventos %8.2lf segundos \n", segundos_v);
+		printf("\n");
 		}
 	} 
-	printf("Tiempo transcurrido Total de ejecución %lf \n", tiempo_tot);
-	printf("Se debió acelerar el enfriamento: %d \n", contae);
-	printf("Se debió acelerar el calentamiento: %d \n", contac);
+	printf("\n\nTiempo transcurrido Total de ejecución %lf \n", tiempo_tot);
+	printf("Se debió acelerar el enfriamento: %d \n", contadorSuperEnfriar);
+	printf("Se debió acelerar el calentamiento: %d \n", contadorSuperCalentar);
 	return(0);
 }		
 int sensa_temp(){			
